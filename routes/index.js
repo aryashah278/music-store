6e5f68986a9626f450526666e11fb2e45779b195
aryashah278 <aryashah97.as@gmail.com>
2020-04-30 02:18:59 -0500

    Checkout, storing orders, forced signin for checkout and profile done

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@ var router = express.Router();
 
 var Product = require('../models/product');
 var Cart = require('../models/cart');
-
+var Order = require('../models/order');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var successMsg = req.flash('success')[0];
   Product.find((error, result) =>{
     if (error) return console.error(error);
-    res.render('shop/index', { title: 'Music Store', products: result });
+    res.render('shop/index', { title: 'Music Store', products: result, successMsg: successMsg, noMessages: !successMsg });
   }).lean()
 });
 
@@ -38,5 +39,28 @@ router.get('/shopping-cart', function(req, res, next){
   res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
+router.get('/checkout', isLoggedIn, function(req,res,next){
+  if(!req.session.cart){
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  var order = new Order({
+    user: req.user,
+    cart: cart
+  });
+  order.save(function(err, result){
+    req.flash('success','Successfully bought products!');
+    req.session.cart = null;
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+      return next();
+  }
+  req.session.oldUrl = req.url;
+  res.redirect('/user/signin');
+}
