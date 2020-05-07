9d6fa189edde2af9b90e4ad98f557faffb96b6bf
aryashah278 <aryashah97.as@gmail.com>
2020-05-06 19:07:17 -0500

    Bugs fixed

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,129 @@ var pages = (req, res, next) => {
   });
 }
 
+
+
+router.get('/add-to-cart/:id', function(req, res, next){
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId, function(err, product){
+    if(err){
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    res.redirect('/');
+  });
+
+});
+
+
+router.get('/shopping-cart/addQty/:id',function(req, res, next){
+  console.log('Add one quantity' + req.params.id);
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart);
+  Product.findById(productId, function(err, product){
+    if(err){
+      return res.redirect('/shopping-cart');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    res.redirect('/shopping-cart');
+  });
+});
+
+router.get('/shopping-cart/reduceQty/:id',function(req, res, next){
+  console.log('Reduce one quantity' + req.params.id);
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart);
+  Product.findById(productId, function(err, product){
+    if(err){
+      return res.redirect('/shopping-cart');
+    }
+    cart.remove(product, product.id);
+    req.session.cart = cart;
+    res.redirect('/shopping-cart');
+  });
+
+});
+
+router.get('/shopping-cart/deleteAllQty/:id',function(req, res, next){
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart);
+  Product.findById(productId, function(err, product){
+    if(err){
+      return res.redirect('/shopping-cart');
+    }
+    cart.removeAll(product, product.id);
+    req.session.cart = cart;
+    res.redirect('/shopping-cart');
+  });
+
+});
+
+router.get('/shopping-cart', function(req, res, next){
+  var errorMsg = req.flash('error')[0];
+  if(!req.session.cart){
+    return res.render('shop/shopping-cart', {products: null});
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice, errorMsg: errorMsg, noMsg : !errorMsg});
+});
+
+router.get('/checkout', isLoggedIn, function(req,res,next){
+  if(!req.session.cart){
+    res.redirect('/shopping-cart');
+  } else{
+    var cart = new Cart(req.session.cart);
+    var order = new Order({
+      user: req.user,
+      cart: cart
+    });
+    
+    var storedItems = cart.items;
+    for (var items in storedItems){
+      var productId = storedItems[items].item._id;
+      var stock = storedItems[items].item.stock;
+      var productQty = storedItems[items].qty;
+      var flashMsgFlag = 0;
+      var errorMessages = "";
+      if(stock-productQty < 0){
+        flashMsgFlag = 1;
+        errorMessages += 'We cannot place the order since we only have ' + stock + ' ' + storedItems[items].item.title + ' left.';
+      }
+    }
+    if(flashMsgFlag){
+      req.flash('error',errorMessages);
+      res.redirect('/shopping-cart');
+    }
+    else{
+      for(var items in storedItems){
+        var productId = storedItems[items].item._id;
+        var stock = storedItems[items].item.stock;
+        var productQty = storedItems[items].qty;
+        stock = stock-productQty;
+      
+        Product.findOneAndUpdate(
+          {_id : productId } ,
+          { stock : stock }, function(err, result){
+            console.log("Successfully updated");
+          }
+        );
+        Product.findById(productId, function(err, product){
+            console.log(product);
+          });
+        order.save(function(err, result){
+          req.flash('success','Successfully bought products!');
+          req.session.cart = null;
+          res.redirect('/');
+        });
+      }
+    }
+  }
+});
+
+
 /* GET home page. */
 router.get('/', pages, function(req, res, next) {
   var successMsg = req.flash('success')[0];
@@ -62,127 +185,6 @@ router.get('/:page', pages, function(req, res, next) {
   }
 });
 
-router.get('/add-to-cart/:id', function(req, res, next){
-  var productId = req.params.id;
-  var cart = new Cart(req.session.cart ? req.session.cart : {});
-
-  Product.findById(productId, function(err, product){
-    if(err){
-      return res.redirect('/');
-    }
-    cart.add(product, product.id);
-    req.session.cart = cart;
-    res.redirect('/');
-  });
-
-});
-
-router.get('/shopping-cart/addQty/:id',function(req, res, next){
-  console.log('Add one quantity' + req.params.id);
-  var productId = req.params.id;
-  var cart = new Cart(req.session.cart);
-  Product.findById(productId, function(err, product){
-    if(err){
-      return res.redirect('/shopping-cart');
-    }
-    cart.add(product, product.id);
-    req.session.cart = cart;
-    res.redirect('/shopping-cart');
-  });
-});
-
-router.get('/shopping-cart/reduceQty/:id',function(req, res, next){
-  console.log('Reduce one quantity' + req.params.id);
-  var productId = req.params.id;
-  var cart = new Cart(req.session.cart);
-  Product.findById(productId, function(err, product){
-    if(err){
-      return res.redirect('/shopping-cart');
-    }
-    cart.remove(product, product.id);
-    req.session.cart = cart;
-    // console.log(req.session.cart);
-    res.redirect('/shopping-cart');
-  });
-
-});
-
-router.get('/shopping-cart/deleteAllQty/:id',function(req, res, next){
-  var productId = req.params.id;
-  var cart = new Cart(req.session.cart);
-  Product.findById(productId, function(err, product){
-    if(err){
-      return res.redirect('/shopping-cart');
-    }
-    cart.removeAll(product, product.id);
-    req.session.cart = cart;
-    // console.log(req.session.cart);
-    res.redirect('/shopping-cart');
-  });
-
-});
-
-
-
-router.get('/shopping-cart', function(req, res, next){
-  var errorMsg = req.flash('error')[0];
-  if(!req.session.cart){
-    return res.render('shop/shopping-cart', {products: null});
-  }
-  var cart = new Cart(req.session.cart);
-  res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice, errorMsg: errorMsg, noMessages : !errorMsg});
-});
-
-router.get('/checkout', isLoggedIn, function(req,res,next){
-  if(!req.session.cart){
-    return res.redirect('/shopping-cart');
-  }
-  var cart = new Cart(req.session.cart);
-  var order = new Order({
-    user: req.user,
-    cart: cart
-  });
-  
-  var storedItems = cart.items;
-  for (var items in storedItems){
-    var productId = storedItems[items].item._id;
-    var stock = storedItems[items].item.stock;
-    var productQty = storedItems[items].qty;
-    var flashMsgFlag = 0;
-    var errorMessages = "";
-    if(stock-productQty < 0){
-      flashMsgFlag = 1;
-      errorMessages += 'We cannot place the order since we only have ' + stock + ' ' + storedItems[items].item.title + ' left.';
-    }
-  }
-  if(flashMsgFlag){
-    req.flash('error',errorMessages);
-    res.redirect('/shopping-cart');
-  }
-  else{
-    for(var items in storedItems){
-      var productId = storedItems[items].item._id;
-      var stock = storedItems[items].item.stock;
-      var productQty = storedItems[items].qty;
-      stock = stock-productQty;
-    
-      Product.findOneAndUpdate(
-        {_id : productId } ,
-        { stock : stock }, function(err, result){
-          console.log("Successfully updated");
-        }
-      );
-      Product.findById(productId, function(err, product){
-          console.log(product);
-        });
-      order.save(function(err, result){
-        req.flash('success','Successfully bought products!');
-        req.session.cart = null;
-        res.redirect('/');
-      });
-    }
-  }
-});
 
 module.exports = router;
 
--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -8,37 +8,83 @@ var products = [
         imagePath : "https://www.worldmusicsupply.com/image/cache/1400x1000/catalog/Jackson/291_6902_574-1400x1000.jpg",
         title: "Jackson Pro Series Monarkh",
         description: "Jackson Pro Series Monarkh SC Ash Ebony Fingerboard Charcoal Ash",
-        price: 749.99,
+        category: "Guitar",
+        price: 750,
         stock: 10
+    }),new Product({
+        imagePath : "https://www.worldmusicsupply.com/image/cache/1100x550/catalog/product_images/ARBW4650RCTK-1__17520-1100x550.jpg",
+        title: "Mapex ARBW4650RCTK Armory Series Drum",
+        description: "Mapex ARBW4650RCTK Armory Series Exterminator Snare Drum 6.85mm Maple - 14x6.5",
+        category: "Drum",
+        price: 259,
+        stock: 4
+    }),
+    new Product({
+        imagePath : "https://www.worldmusicsupply.com/image/cache/1400x1000/catalog/product_images/095-5500-021__37773-1400x1000.jpg",
+        title: "Fender FB-55 Banjo",
+        description: "Fender FB-55 5 String Banjo Natural Finish",
+        category: "Banjo",
+        price: 550,
+        stock: 5
+    }),
+    new Product({
+        imagePath : "https://www.worldmusicsupply.com/image/cache/1400x1000/catalog/Washburn/M3SWK-1400x1000.jpg",
+        title: "Demo - Washburn M3SWK-D F-Style Mandolin",
+        description: "Demo - Washburn M3SWK-D F-Style Mandolin Carved Solid Sitka Spruce Top",
+        category: "Mandolin",
+        price: 649,
+        stock: 6
     }),
     new Product({
         imagePath : "https://www.worldmusicsupply.com/image/cache/1400x1000/catalog/Charvel/296_9041_576-1400x1000.jpg",
         title: "Charvel Pro Mod DK24",
         description: "Charvel Pro Mod DK24 HH FR Maple Fingerboard Snow White",
+        category: "Guitar",
         price: 850,
         stock: 5
-    }),new Product({
+    }),
+    new Product({
+        imagePath : "https://www.worldmusicsupply.com/image/cache/1100x550/catalog/product_images/_1269608497__32310-1100x550.jpg",
+        title: "Mapex MPBC4550CXN MPX Series Drum",
+        description: "Mapex MPBC4550CXN MPX Series Birch Snare Drum in Natural Gloss",
+        category: "Drum",
+        price: 139,
+        stock: 6
+    }),
+    new Product({
+        imagePath : "https://www.worldmusicsupply.com/image/cache/1400x1000/catalog/B10-1400x1000.jpg",
+        title: "Washburn B10 5-string Banjo",
+        description: "Washburn B10 5-string Banjo Mahogany back and sides Sunburst Gloss",
+        category: "Banjo",
+        price: 139,
+        stock: 6
+    }),
+    new Product({
         imagePath : "https://www.worldmusicsupply.com/image/cache/1400x1000/catalog/Fender/IMG_6719-1400x1000.jpg",
         title: "Fender American Original 50s Telecaster",
         description: "Fender American Original 50s Telecaster Maple Fingerboard Butterscotch Blonde",
+        category: "Guitar",
         price: 1850,
         stock: 6
     }),new Product({
         imagePath : "https://www.worldmusicsupply.com/image/cache/1100x550/catalog/Fender/DSC_0035-1100x550.jpg",
         title: "Fender American Professional Stratocaster",
         description: "Fender American Professional Stratocaster Electric Guitar Rosewood Fingerboard Sonic Gray SN# US19094746",
+        category: "Guitar",
         price: 1450,
         stock: 2
     }),new Product({
         imagePath : "https://www.worldmusicsupply.com/image/cache/1100x550/catalog/ESP/EC1000FM_STP-1100x550.jpg",
         title: "Esp Ltd EC1000 Flame",
         description: "Esp Ltd EC1000 Flame Top See Thru Purple Electric Guitar Seymour Duncan Pickups",
+        category: "Guitar",
         price: 999,
         stock: 3
     }),new Product({
         imagePath : "https://www.worldmusicsupply.com/image/cache/1400x1000/catalog/product_images/Untitled_2__28978-1400x1000.jpg",
         title: "ESP Ltd EC256 Electric Guitar",
         description: "ESP Ltd EC256 Electric Guitar Lemon Drop",
+        category: "Guitar",
         price: 400,
         stock: 4
     }),   
