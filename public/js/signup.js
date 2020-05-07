6f2f6503dcc3eb17d625bf8fd195bc0ae13724fe
Mihir Hindocha <mihirhindocha97@gmail.com>
2020-05-07 16:53:56 -0500

    signin and signup form validation done

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,52 +2,127 @@ $(document).ready(function () {
 
     var strength = "";
 
-    $('#password').focusin(function () {
-        $('#submit-message').hide();
-    })
-    $('#password').keyup(function () {
+    var email = $("#email");
+    var emailMessage = $("#email-message");
+    var signUpPassword = $('#signup-password');
+    var signInPassword = $("#signin-password");
+    var passwordMessage = $('#password-message');
+    var submit = $("#submit");
+    var submitMessage = $('#submit-message');
+    var emailOk = $("#email-ok");
+    var passwordOk = $("#password-ok");
+    var passwordRequirementMessage = "Minimum Password length is 6 and it should include alphabets, numbers and special characters";
+
+    emailOk.hide();
+    passwordOk.hide();
+
+    signUpPassword.focusin(function () {
+        submitMessage.hide();
+    });
+
+    signInPassword.focusin(function () {
+        passwordMessage.hide();
+    });
+
+    email.focusin(function () {
+        emailMessage.hide();
+    });
+
+    signUpPassword.blur(function () {
+        if (signUpPassword.val().length == 0) {
+
+            passwordOk.hide();
+            passwordMessage.show()
+                .removeClass()
+                .addClass('error')
+                .html("Password cannot be empty!");
+        }
+    });
+
+    signInPassword.blur(function () {
+        if (signInPassword.val().length == 0) {
+
+            passwordMessage.show()
+                .removeClass()
+                .addClass('error')
+                .html("Password cannot be empty!");
+        }
+    });
+
+    signUpPassword.keyup(function () {
         var number = /([0-9])/;
         var alphabets = /([a-zA-Z])/;
         var special_characters = /([~,!,@,#,$,%,^,&,*,-,_,+,=,?,>,<])/;
 
-        $('#password-strength-message').show();
+        passwordMessage.show();
 
-        if ($('#password').val().length == 0) {
+        if (signUpPassword.val().length == 0) {
+
+            passwordOk.hide();
             strength = "";
-            $('#password-strength-message').hide();
+            passwordMessage.hide();
         }
 
-        if ($('#password').val().length < 6) {
-            $('#password-strength-message').removeClass();
-            $('#password-strength-message').addClass('weak-password');
-            $('#password-strength-message').html("Weak (should be at least 6 characters.)");
+        if (signUpPassword.val().length < 6) {
+
+            passwordOk.hide();
+            passwordMessage.removeClass()
+                .addClass('weak-password')
+                .html(passwordRequirementMessage);
             strength = "weak";
         } else {
-            if ($('#password').val().match(number) && $('#password').val().match(alphabets) && $('#password').val().match(special_characters)) {
-                $('#password-strength-message').removeClass();
-                $('#password-strength-message').addClass('strong-password');
-                $('#password-strength-message').html("Strong");
+            if (signUpPassword.val().match(number) && signUpPassword.val().match(alphabets) && signUpPassword.val().match(special_characters)) {
+
+                passwordOk.show();
+                passwordMessage.hide();
                 strength = "strong";
             } else {
-                $('#password-strength-message').removeClass();
-                $('#password-strength-message').addClass('medium-password');
-                $('#password-strength-message').html("Medium (should include alphabets, numbers and special characters.)");
+
+                passwordOk.hide();
+                passwordMessage.removeClass()
+                    .addClass('medium-password')
+                    .html(passwordRequirementMessage);
                 strength = "medium";
             }
         }
     });
 
-    $("#submit").click(function () {
-        if (strength == "strong"){
+    submit.click(function () {
+        if (strength == "strong") {
             return true;
-        } else{
-            $('#submit-message').show();
-            $('#submit-message').removeClass();
-            $('#submit-message').addClass('weak-password');
-            $('#submit-message').html("Password should be strong to sign up!");
+        } else {
+            submitMessage.show()
+                .removeClass()
+                .addClass('error')
+                .html("Password should be strong to sign up!");
             return false;
         }
     })
+
+    email.focusin(function () {
+        emailMessage.hide();
+    })
+
+    email.blur(function () {
+
+        if (email.val().length == 0) {
+
+            emailOk.hide();
+            emailMessage.show()
+                .removeClass()
+                .addClass('error')
+                .html("Email cannot be empty!");
+        } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.val())) {
+
+            emailOk.hide();
+            emailMessage.show()
+                .removeClass()
+                .addClass('error')
+                .html("Please enter a valid email");
+        } else {
+            emailOk.show();
+        }
+    });
 
 
 });
